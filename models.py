bd3222b1677d996780d88e599b1dd1140b17ed4d
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-28 15:10:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-
 from flask_login import UserMixin
-
+from sqlalchemy import Column, Integer, String
 from app import db
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256))
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password_hash = Column(String(256))
