daf94b2039421fab8cc45af43995ba3315e60983
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-28 15:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# Import models after db is initialized to avoid circular import
 from models import User
 
 @login_manager.user_loader
